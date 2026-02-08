700a49b8c9744086e2b593a93af4fc4073736d64
Thangatamil <thangatamil518@gmail.com>
2026-02-08 15:58:28 +0530

    key remaps added

--- lua/classic/remap.lua
+++ lua/classic/remap.lua
@@ -1,3 +1,7 @@
-vim.keymap.set('n', '<leader>o', '<cmd>Ex<CR>', { noremap = true, desc = 'Ex' })
+vim.keymap.set('n', '<leader>o', '<cmd>Ex<cr>', { noremap = true, desc = 'ex' })
+
+vim.keymap.set("i", "<C-j>", "<Esc>", { noremap = true, desc = "Exit insert mode with Ctrl + j" })
+
+vim.keymap.set("n", "yc", "yy<cmd>normal gcc<CR>p", { noremap = true, desc = "Duplicate line and comment original" })
 
 
--- lua/classic/set.lua
+++ lua/classic/set.lua
@@ -1,13 +1,7 @@
-vim.cmd("set number")
+vim.g.mapleader= " "
 
--- Normal mode: Block cursor (no blink)  
-vim.cmd("set guicursor=n-v-c:block-Cursor/lCursor,ve:ver35-Cursor,o:hor50-Cursor  ")
--- Visual mode: Reverse block (inverted colors)  
-vim.cmd("set guicursor+=v:block-CursorReverse")
--- Insert mode: Vertical beam (25% width)  
+vim.cmd("set number")
 vim.cmd("set guicursor+=i-ci:block")
--- vim.cmd("set guicursor=n-v-c-i:block")
 
 vim.opt.clipboard = "unnamedplus"
 
-vim.g.mapleader= " "
